faae5a82b9c323fde43211b1f27bae34fddedacc
Shanley Caswell <shanleycaswell@gmail.com>
2023-06-06 10:39:37 -0400

    new html and js for form

--- index.js
+++ index.js
@@ -22,16 +22,16 @@ function renderMenuItem(foodItem){
     //call dragDrop
     //call addNutrition
 }
-function createFood(foodArray) {
-foodArray.forEach((element) => {
-let foodDiv = document.getElementById('food-menu')
-let foodItem = document.createElement('img')
-foodItem.classList.add('foods')
-foodItem.src = element.image_url
-foodDiv.appendChild(foodItem)   
-});
 
-}
+function createFood(foodArray) {
+    foodArray.forEach((element) => {
+    let foodDiv = document.getElementById('food-menu')
+    let foodItem = document.createElement('img')
+    foodItem.classList.add('foods')
+    foodItem.src = element.image_url
+    foodDiv.appendChild(foodItem)   
+    });
+}   
 //drag and drop feature -- P
 function dragDrop(foodItemElement){
 }
@@ -44,7 +44,20 @@ function addNutrition(foodItem){
 
 //food form -- S
 foodForm.addEventListener('submit', (e) =>{
-    
+    e.preventDefault()
+    const newFood = {
+        name: e.target.name.value,
+        image_url: e.target["image-input"].value,
+        nutrition:{
+            calories: e.target.servings.value,
+            sugar: e.target.calories.value,
+            fat: e.target.fat.value,
+            protein: e.target.protein.value,
+            "serving-size": e.target.servings.value
+        },
+        nutrition_url: e.target["nutrition_url"].value
+    }
+
     
     
 })
