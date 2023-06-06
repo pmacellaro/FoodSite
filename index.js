a8f92b5deb64734ebab5dbc4d253a704dd966070
bonbongiraffe <francescowai@yahoo.com>
2023-06-06 10:56:13 -0400

    add render nutrition total

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const foodMenu = document.getElementById('food-menu') //shanley
 const nutritionBox = document.getElementById('nutrition-total') //frankie
 const facts = document.getElementById('facts') 
 const foodForm = document.getElementById('food-form') //shanley
+//nutrition total list elements
+const totalCal = document.getElementById('calories-total')
+const totalSug = document.getElementById('sugar-total')
+const totalFat = document.getElementById('fat-total')
+const totalPro = document.getElementById('protein-total')
+const totalSS = document.getElementById('serving-size-total')
+//nutrition total
 let nutritionTotal = {calories: 0, fat: 0, protein: 0, "serving-size": 0, sugar: 0}
 let testFoodObj
 
@@ -44,6 +51,16 @@ function dragDrop(foodItemElement){
 function addNutrition(foodItem){
     Object.keys(foodItem.nutrition).forEach((key) => 
         nutritionTotal[key] += foodItem.nutrition[key])
+    renderNutrition(foodItem)
+}
+
+//render nutrition total -- F
+function renderNutrition(foodItem){
+    totalCal.textContent = `Calories: ${nutritionTotal.calories} cal`
+    totalSug.textContent = `Sugar: ${nutritionTotal.sugar} g`
+    totalFat.textContent = `Fat: ${nutritionTotal.fat} g`
+    totalPro.textContent = `Protein: ${nutritionTotal.protein} g`
+    totalSS.textContent = `Serving Amount: ${nutritionTotal["serving-size"]} g`
 }
 
 //facts -- F
